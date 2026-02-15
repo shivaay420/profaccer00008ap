88c5877c94a4ec378cd5f466022c31145329604e
shivaay420 <67571175+shivaay420@users.noreply.github.com>
2026-02-15 19:26:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post("/transfer", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
